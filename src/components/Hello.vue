12ab90d25d7cdbfdcd212b4090d872281ac83f99
Leon Harper <leon@ecowebhosting.co.uk>
2018-07-21 02:08:39 +0100

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="app">
+        <h1>Ore Reserves: {{ ore }}</h1>
+        <br>
+        <button v-on:click="mineOre">Mine Ore</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      ore: 0
+    }
+  },
+  methods: {
+    mineOre: function () {
+      this.ore++
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
