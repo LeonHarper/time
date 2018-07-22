b5477413b071c6b36b5f9e3927992dc1ed01dfa8
Leon Harper <leon@ecowebhosting.co.uk>
2018-07-22 16:41:50 +0100

    removed vue logo, add happiness and energy counts

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,9 +1,13 @@
 <template>
     <div id="app">
         <h1>{{days}} days {{hours}} hours {{minutes}} minutes and {{ seconds }} seconds</h1>
+        <p>happiness : {{happiness}}</p>
+        <p>energy : {{energy}}</p>
         <p>ticks : {{ticks}}</p>
         <br>
-        <button v-on:click="tickClock">Tick Clock</button>
+        <button v-on:click="sleepNight">Sleep</button>
+        <br>
+        <button v-on:click="play">Play</button>
     </div>
 </template>
 
@@ -12,12 +16,14 @@ export default {
   name: 'hello',
   data () {
     return {
-      ticks: 2160067600,
+      ticks: 0,
       seconds: 0,
       minutes: 0,
       hours: 0,
       days: 0,
-      weeks: 0
+      weeks: 0,
+      happiness: 60,
+      energy: 57600
     }
   },
   methods: {
@@ -27,9 +33,17 @@ export default {
       this.minutes = Math.floor(this.ticks / 60) % 60
       this.hours = Math.floor(Math.floor(this.ticks / 60) / 60) % 24
       this.days = Math.floor(Math.floor(Math.floor(this.ticks / 60) / 60) / 24)
+      this.energy = this.energy - 1
+      this.happiness = this.happiness - 1
     },
-    sleep: function () {
-      this.hours = this.hours + 8
+    sleepNight: function () {
+      this.ticks = this.ticks + 28800
+      this.energy = this.energy + 57600
+    },
+    play: function () {
+      this.ticks = this.ticks + 7200
+      this.energy = this.energy - 7200
+      this.happiness = this.happiness + 10
     }
   },
   mounted () {
